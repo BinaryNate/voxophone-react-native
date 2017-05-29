a75b966fd2ceaa37796bf5916754b974a67f76d8
Nathan Hill <nathan.hill@inin.com>
2017-05-29 11:44:59 -0400

    Removed old, unneeded code from music-note-meter.

--- app/components/music-note-meter/index.js
+++ app/components/music-note-meter/index.js
@@ -7,40 +7,10 @@ import { MusicNoteEventType } from 'react-native-voxophone-engine';
 import delay from '../../utils/delay';
 import Ring from '../music-note-meter-ring';
 
-const NUMBER_OF_RINGS = 8;
-const NOTE_ON_TRANSITION_MILLISECONDS = 40;
-const NOTE_OFF_TRANSITION_MILLISECONDS = 100;
-const TOTAL_HUE_LIGHTNESS_CHANGE = 0.5;
-const RANDOM_COLORS_ENABLED = true;
-
-const meterCenterDiameter = 140;
-
-const styles = StyleSheet.create({
-
-    musicNoteMeter: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        // backgroundColor: 'green',
-        // borderRadius: 170
-    },
-    meterCenter: {
-        flexGrow: 0,
-        flexShrink: 0,
-        flexBasis: 'auto',
-        height: meterCenterDiameter,
-        width: meterCenterDiameter,
-        borderRadius: meterCenterDiameter / 2,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    meterCenterText: {
-        color: color1,
-        fontSize: 72,
-        fontWeight: 'bold'
-    }
-});
+const NUMBER_OF_RINGS = 8,
+      NOTE_ON_TRANSITION_MILLISECONDS = 40,
+      NOTE_OFF_TRANSITION_MILLISECONDS = 100,
+      METER_CENTER_DIAMETER = 140;
 
 /**
 * Illustrates the volume envelope of a note played using concentric circles and
@@ -66,7 +36,7 @@ export default class MusicNoteMeter extends Component {
         return (
             <View style={styles.musicNoteMeter}>
 
-                {this._renderRings(this.state.rings, this.props.diameter, meterCenterDiameter,
+                {this._renderRings(this.state.rings, this.props.diameter, METER_CENTER_DIAMETER,
                     <View style={styles.meterCenter}>
                         <Text style={styles.meterCenterText}>C#</Text>
                     </View>
@@ -159,137 +129,37 @@ export default class MusicNoteMeter extends Component {
         }, Promise.resolve());
     }
 
-    /**
-    * Creates and hooks up the ring-shaped views arranged in concentric circles that are used in the UI.
-    *
-    * @returns {Promise.<Array.<View>>} - Array where each item is a View that is a ring in the music note meter's UI.
-    *                                     The rings go from the center to the circumference as the index increases.
-    */
-    _createAndConfigureRings() {
-
-        return this._getOuterRing()
-        .then(outerRing => {
-
-            let center = this.view.getViewById('meter-center');
-            this.view.removeChild(center);
-
-            let radiusLengthToDivide = outerRing.borderRadius - center.borderRadius;
-            let radiusStepSize = radiusLengthToDivide / NUMBER_OF_RINGS;
-
-            let numberOfRingsToCreate = NUMBER_OF_RINGS - 1; // Subtract one, since the outer ring already exists.
-            let rings = this._createRings(center, numberOfRingsToCreate, radiusStepSize);
-            rings.push(outerRing);
-
-            // Link the rings together in a parent-child hierarchy.
-            this._linkRingsTogether(rings);
-            rings[0].addChild(center);
-
-            this._setRingColors(rings);
-            return rings;
-        })
-        .catch(error => this._logger.error(`An error occurred during ring creation.`, { error }));
-    }
-
-    /**
-    * @param   {ui/View}        centerRing     - The initial, center ring on which the size of the other rings will be based.
-    * @param   {int}            numberOfRings  - Total number of rings to render
-    * @param   {int}            radiusStepSize - The distance in pixels between the border of a ring and the border of the ring
-    *                                            immediately outside of it.
-    * @returns {Array.<ui/View}
-    */
-    _createRings(centerRing, numberOfRings, radiusStepSize) {
-
-        let previousRing = centerRing,
-            rings = [];
-
-        for (let i = 0; i < numberOfRings; i++) {
-
-            let radius = previousRing.borderRadius + radiusStepSize;
-            let ring = this._createRing(radius);
-            rings.push(ring);
-            previousRing = ring;
-        }
-        return rings;
-    }
-
-    _createRing(radius) {
-
-        let ring = new FlexboxLayout();
-        ring.height = ring.width = radius * 2;
-        ring.borderRadius = radius;
-        ring.alignItems = AlignItems.CENTER;
-        ring.justifyContent = JustifyContent.CENTER;
-        FlexboxLayout.setFlexGrow(ring, 0);
-        FlexboxLayout.setFlexShrink(ring, 0);
-        return ring;
-    }
-
-    /**
-    * Gets a reference to the existing view which will serve as the outermost ring and
-    * then configures and returns it.
-    *
-    * @returns {Promise.<ui/View>}
-    */
-    _getOuterRing() {
-
-        return Promise.resolve()
-        .then(() => {
-
-            let outerRing = this.view;
-            let outerRingSize = outerRing.getActualSize();
-            let outerRingRadius = Math.min(outerRingSize.height, outerRingSize.width) / 2;
-
-            if (outerRingRadius === 0) {
-                // The view hasn't been fully initialized. Let's try again in 1 millisecond.
-                return delay(1).then(() => this._getOuterRing());
-            }
-            // Set the height and width equal to each other, because this view is initially just a rectangle.
-            outerRing.height = this.view.width = outerRingRadius * 2;
-            outerRing.borderRadius = outerRingRadius;
-            return outerRing;
-        });
-    }
-
     _getRandomColor() {
 
         let num = Math.floor(Math.random() * 0xFFFFFF);
         let hex = num.toString(16).padStart(6, '0');
         return '#' + hex;
     }
-
-    _linkRingsTogether(rings) {
-
-        rings.forEach((ring, index) => {
-            let parentRing = rings[index + 1];
-
-            if (parentRing) {
-                parentRing.addChild(ring);
-            }
-        });
-    }
-
-    _setRingColors(rings) {
-
-        rings.forEach((ring, index) => {
-            ring.backgroundColor = this._getColorForRingIndex(index);
-        });
-    }
-
-    _getColorForRing(ring) {
-
-        let ringIndex = this._rings.indexOf(ring);
-        return this._getColorForRingIndex(ringIndex);
-    }
-
-    _getColorForRingIndex(ringIndex) {
-
-        if (RANDOM_COLORS_ENABLED) {
-            return this._getRandomColor();
-        }
-
-        let lightnessStepSize = TOTAL_HUE_LIGHTNESS_CHANGE / NUMBER_OF_RINGS;
-        let rootColor = '#ff871e';
-        let { r, g, b } = ColorEditor(rootColor).darken(ringIndex * lightnessStepSize).rgb().object();
-        return new Color(255, r, g, b);
-    }
 }
+
+const styles = StyleSheet.create({
+
+    musicNoteMeter: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // backgroundColor: 'green',
+        // borderRadius: 170
+    },
+    meterCenter: {
+        flexGrow: 0,
+        flexShrink: 0,
+        flexBasis: 'auto',
+        height: METER_CENTER_DIAMETER,
+        width: METER_CENTER_DIAMETER,
+        borderRadius: METER_CENTER_DIAMETER / 2,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    meterCenterText: {
+        color: color1,
+        fontSize: 72,
+        fontWeight: 'bold'
+    }
+});
