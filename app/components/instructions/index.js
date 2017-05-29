9360f68286a36ae8c9199b3046327aef899f2f32
Nathan Hill <nathan.hill@inin.com>
2017-05-29 15:14:18 -0400

    Now have the animation on the instruction page working, although it's currently a bit shaky.

--- app/components/instructions/index.js
+++ app/components/instructions/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Animated } from 'react-native';
 import { validate } from 'parameter-validator';
 
-console.disableYellowBox = true;
+const MARGIN_MIN = 20;
+const MARGIN_MAX = 50;
+const MARGIN_ANIMATION_DURATION = 4000;
 
 export default class Instructions extends Component {
 
@@ -10,6 +12,25 @@ export default class Instructions extends Component {
 
         super(props);
         validate(props.dependencies, [ 'logger', 'voxophone' ], this, { addPrefix: '_' });
+
+        this.state = {
+            margin: new Animated.Value(MARGIN_MIN)
+        };
+
+        let cycleAnimation = () => {
+            Animated.sequence([
+                Animated.timing(this.state.margin, {
+                    toValue: MARGIN_MAX,
+                    duration: MARGIN_ANIMATION_DURATION
+                }),
+                Animated.timing(this.state.margin, {
+                    toValue: MARGIN_MIN,
+                    duration: MARGIN_ANIMATION_DURATION
+                })
+            ]).start(() => cycleAnimation());
+        }
+
+        cycleAnimation();
     }
 
     render() {
@@ -17,7 +38,7 @@ export default class Instructions extends Component {
         return (
             <View style={styles.view}>
                 <View style={styles.instructionsContainer}>
-                    <Image style={styles.instructions} source={require('./img/instructions-phone-down.png')}/>
+                    <Animated.Image style={[ styles.instructions, { margin: this.state.margin }]} source={require('./img/instructions-phone-down.png')}/>
                 </View>
                 <View style={styles.okButtonContainer}>
                     <Image style={styles.okButton} source={require('./img/check-mark-button.png')}/>
